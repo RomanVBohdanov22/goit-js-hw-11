e13feb1d8e7b2658ed322ff47f46c911085d3bb2
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-06 20:24:49 +0000

    Deploying to gh-pages from @ RomanVBohdanov22/goit-js-hw-11@85f46dd15250123f6a0faca0300293e65f7a9cb0 🚀

---
+++ index.602ecc13.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search-form");console.log(e),e.addEventListener("submit",(function(e){e.preventDefault()}));
+//# sourceMappingURL=index.602ecc13.js.map
---
+++ index.7946dea6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-form");console.log(e),e.addEventListener("submit",(function(e){e.preventDefault()}))}();
+//# sourceMappingURL=index.7946dea6.js.map
