64048ba6b523540ed55628b852c63623781d32b9
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-07 16:56:14 +0200

    New file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formLnk = document.querySelector(".search-form");
-
+const gallery = document.querySelector('.gallery');
 //console.log(formLnk);
 
 formLnk.addEventListener("submit", onSubmitBtn);
@@ -18,6 +18,55 @@ function onSubmitBtn(e) {
     console.log(`inputLine ${searchQuery.value}`);
     //e.currentTarget.reset();
 }
+
+/*<!-- 
+<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>
+
+-->*/
+
+/*
+const galleryMurkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li>
+    <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+    </li>
+`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMurkup);
+
+console.log(gallery);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+ */
+
 
 /**
  const { height: cardHeight } = document
