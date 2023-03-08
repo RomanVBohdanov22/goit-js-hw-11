0cc9b27dce6f9a29c552823087cac0c432b5611c
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-08 12:27:25 +0200

    Khvi

--- src/js/galleryFetch.js
+++ src/js/galleryFetch.js
@@ -1,6 +1,26 @@
 KEY = '34203020 - 9ccd90725bbcf7c5b689f6c58';
-BASEURL = 'https://pixabay.com/api/';
+BASE_URL = 'https://pixabay.com/api/';
 
 export function galleryFetch(line) { 
-    
+
+    const searchUrl = BASE_URL + line + FIELDS;
 }
+
+//https://pixabay.com/api/?key=34203020-9ccd90725bbcf7c5b689f6c58&q=yellow+flowers&image_type=photo
+
+
+/*
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(posts => console.log(posts))
+  .catch(error => console.log(error));
+*/
+/*
+// Change this number to fetch different post
+const postId = 1;
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(error => console.log(error));
+ */
