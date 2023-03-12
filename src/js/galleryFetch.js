8c1004674ca0a7a7b4323a89532779ea35883669
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-03-12 14:54:33 +0200

    better then ever params

--- src/js/galleryFetch.js
+++ src/js/galleryFetch.js
@@ -2,18 +2,23 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/?key=';
 const KEY = '34203020-9ccd90725bbcf7c5b689f6c58';
-const ABS_FIELFS = '&image_type=photo&orientation=horizontal&safesearch=true';
-/*URLSearchParams {
-
- }*/
+let PER_PAGE = 40;
 
 export async function galleryFetch(queryLine, currentPage) { 
-    let PER_PAGE = 40;
-    let FIELDS = ABS_FIELFS+`&page=${currentPage}&per_page=${PER_PAGE}`;
-    const searchUrl = BASE_URL + KEY + '&q=' + queryLine + FIELDS;
-    console.log(searchUrl);
+
     try {
-        let response = await axios.get(searchUrl);
+        let response = await axios.get(
+            `${BASE_URL}${KEY}`, {
+                params: {
+                    q: `${queryLine}`,
+                    orientation: `horizontal`,
+                    image_type: `photo`,                    
+                    page: currentPage,
+                    per_page: PER_PAGE,
+                    safesearch: `true`,
+                }
+            }
+        ); 
         return response;
     }
     catch (e) { 
